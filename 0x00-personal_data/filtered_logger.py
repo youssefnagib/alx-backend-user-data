c1073421844ed3f53a5c30465db39fbf1cfb6432
youssef nagib <youssefnagib14@yahoo.com>
2024-11-07 16:47:28 +0200

    logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -16,3 +16,23 @@ def filter_datum(
     '''Filter data'''
     pattern = f"({'|'.join(fields)})=[^\\{separator}]*"
     return re.sub(pattern, lambda m: f"{m.group(1)}={redaction}", message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        record.msg = filter_datum(self.fields,
+                                  self.REDACTION,
+                                  record.getMessage(),
+                                  self.SEPARATOR)
+        return super().format(record)
