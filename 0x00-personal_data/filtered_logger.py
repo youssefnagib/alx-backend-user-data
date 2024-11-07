e51f22862aedcadd9004d7091cf12c3470dbea21
youssef nagib <youssefnagib14@yahoo.com>
2024-11-07 19:02:58 +0200

    logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -55,7 +55,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db()  -> mysql.connector.connection.MySQLConnection:
     """Returns a database connection"""
     user = os.getenv("PERSONAL_DATA_DB_USERNAME", "root")
     password = os.getenv("PERSONAL_DATA_DB_PASSWORD", "")
