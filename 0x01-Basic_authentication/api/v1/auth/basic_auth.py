33482d3f6d04550aa121db80ef7ce5fa6785cad7
youssef nagib <youssefnagib14@yahoo.com>
2024-11-12 23:19:38 +0200

    api

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 # api/v1/auth/basic_auth.py
-
+''' The basic authentication method'''
 from api.v1.auth.auth import Auth
+
 
 class BasicAuth(Auth):
     """ BasicAuth class that inherits from Auth """
