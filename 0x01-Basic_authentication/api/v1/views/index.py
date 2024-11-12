72c7544101557f81ceb06f28e442e2f6a19eb743
youssef nagib <youssefnagib14@yahoo.com>
2024-11-12 16:53:07 +0200

    api

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -24,3 +24,12 @@ def stats() -> str:
     stats = {}
     stats['users'] = User.count()
     return jsonify(stats)
+
+
+@app_views.route('/unauthorized',  methods=['GET'], strict_slashes=False)
+def test_unauthorized() -> None:
+  """ GET /api/v1/unauthorized
+    Return:
+      - an unauthorized error message
+  """
+  abort(401)
