617a2d01ba2b8cdafe0fe20c9a0bd52e5efbddc7
youssef nagib <youssefnagib14@yahoo.com>
2024-11-13 00:13:23 +0200

    api

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -6,6 +6,7 @@ from api.v1.auth.auth import Auth
 class BasicAuth(Auth):
     """ BasicAuth class that inherits from Auth """
     def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        ''' Extract base64 authorization header'''
         if authorization_header is None:
             return None
         if not isinstance(authorization_header, str):
