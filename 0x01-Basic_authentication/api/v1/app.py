a30a8fb277a4fa99f5dff8dac4aa57aa18dc1f69
youssef nagib <youssefnagib14@yahoo.com>
2024-11-12 23:18:14 +0200

    api

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -7,15 +7,19 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
+from typing import Union
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-auth = None
-if os.getenv("AUTH_TYPE") == "auth":
-    from api.v1.auth.auth import Auth
+auth: Union[Auth, None] = None
+if os.getenv("AUTH_TYPE") == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif os.getenv("AUTH_TYPE") == "auth":
     auth = Auth()
 
 
---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,7 @@
+# api/v1/auth/basic_auth.py
+
+from api.v1.auth.auth import Auth
+
+class BasicAuth(Auth):
+    """ BasicAuth class that inherits from Auth """
+    pass
