e5155f3aae5a5113bcb43e9bb0e572a42cc7ba60
youssef nagib <youssefnagib14@yahoo.com>
2024-11-13 00:39:26 +0200

    api

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -15,3 +15,16 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header[len("Basic "):]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        ''' Decode base64 authorization header'''
+        import base64
+        if not base64_authorization_header:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decoded_bytes = base64.b64decode(base64_authorization_header)
+            return decoded_bytes.decode("utf-8")
+        except Exception:
+            return None
