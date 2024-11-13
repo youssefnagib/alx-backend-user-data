79e58c30a535285c607fcbe2046650e601cfa73f
youssef nagib <youssefnagib14@yahoo.com>
2024-11-13 02:18:51 +0200

    api

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 ''' The basic authentication method'''
 from api.v1.auth.auth import Auth
+from typing import TypeVar
+from models.user import User
 
 
 class BasicAuth(Auth):
@@ -44,3 +46,22 @@ class BasicAuth(Auth):
         if len(credentials) != 2:
             return None, None
         return credentials[0], credentials[1]
+
+    def user_object_from_credentials(self,
+                                     user_email: str,
+                                     user_pwd: str) -> TypeVar('User'):
+        ''' Create a User object from user email and password'''
+        if user_email is None or not isinstance(user_email, str):
+            return None
+        if user_pwd is None or not isinstance(user_pwd, str):
+            return None
+
+        users = User.search({'email': user_email})
+        if not users:
+            return None
+
+        user = users[0]
+        if not user.is_valid_password(user_pwd):
+            return None
+
+        return user
