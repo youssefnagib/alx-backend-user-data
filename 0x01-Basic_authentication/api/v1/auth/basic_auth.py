52a3d452abd2aaad6fbe325cc834ac2de98a74ad
youssef nagib <youssefnagib14@yahoo.com>
2024-11-13 02:51:05 +0200

    api

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -68,3 +68,28 @@ class BasicAuth(Auth):
             return user
 
         return None
+    
+    def current_user(self, request=None) -> TypeVar('User'):
+        ''' Retrieve the current authenticated user from the request'''
+        if request is None:
+            return None
+
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+
+        base64_auth_header = self.extract_base64_authorization_header(
+            auth_header)
+        if base64_auth_header is None:
+            return None
+
+        decoded_auth = self.decode_base64_authorization_header(base64_auth_header)
+        if decoded_auth is None:
+            return None
+
+        user_email, user_pwd = self.extract_user_credentials(
+            decoded_auth)
+        if user_email is None or user_pwd is None:
+            return None
+
+        return self.user_object_from_credentials(user_email, user_pwd)
