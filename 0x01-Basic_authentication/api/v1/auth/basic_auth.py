cb3f0340f00c107fb157bb8cef8b2d683bd69601
youssef nagib <youssefnagib14@yahoo.com>
2024-11-13 03:04:45 +0200

    api

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -42,7 +42,7 @@ class BasicAuth(Auth):
             return None, None
         if not isinstance(decoded_base64_authorization_header, str):
             return None, None
-        credentials = decoded_base64_authorization_header.split(":")
+        credentials = decoded_base64_authorization_header.split(":", 1)
         if len(credentials) != 2:
             return None, None
         return credentials[0], credentials[1]
